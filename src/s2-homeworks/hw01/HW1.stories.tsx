2512552246463ba460833f2fa56a24a28acf8ab9
Vad <ichetowwwkin@mail.ru>
2024-07-04 23:27:57 +0300

    first commit

---
+++ src/s2-homeworks/hw01/HW1.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HW1 from './HW1';
+
+
+
+const meta: Meta<typeof HW1> = {
+  component: HW1,
+};
+
+export default meta;
+
+
+export const My = () =>{
+    return <div>Hello</div>
+}
