800246e468ab122cf2f1ea44a8b24c6c62f397d9
Vad <ichetowwwkin@mail.ru>
2024-07-14 15:01:52 +0300

    h8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: any, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let c = [...state]
 
-            return state // need to fix
+            return action.payload === 'up' ? c.sort((a, b) => {
+                if (a.name < b.name) {
+                    return -1
+                } else if (a.name > b.name) {
+                    return 1
+                }
+                return 0;
+
+            }) : c.sort((a, b) => {
+                if (a.name > b.name) {
+                    return -1
+                } else if (a.name < b.name) {
+                    return 1
+                }
+                return 0;
+            })
+
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter((it: UserType) => it.age >= action.payload)
         }
         default:
             return state
